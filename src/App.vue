c9a7510e89af9fcabe85449e2051edd678077fd2
malte <skyguardian42@gmail.com>
2018-08-23 16:10:31 +0200

    interface

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
+  <div class="settings">
+    <input type="radio" id="caesar" value="caesar" name="method" v-model="method">
+    <label for="caesar">Caesar</label>
+    
+    <input type="radio" id="vigenere" value="vigenere" name="method" v-model="method">
+    <label for="vigenere">Vigenere</label>
+  </div>
 
-		<select name="method" v-model="method">
-			<option>Caesar</option>
-			<option>Vigenere</option>
-		</select>
+  <div class="settings">
+    <input type="radio" id="encrypt" value="encrypt" name="direction" v-model="direction">
+    <label for="encrypt">Verschlüsseln</label>
+    
+    <input type="radio" id="decrypt" value="decrypt" name="direction" v-model="direction">
+    <label for="decrypt">Entschlüsseln</label>
+  </div>
+
+  <p>Picked: {{ method }}</p>
+  <p>Picked: {{ direction }}</p>
 
 
+  <textarea v-model="key" placeholder="Schlüssel"></textarea>
+
+  <textarea v-model="input" placeholder="Input"></textarea>
+
+  <p> {{ output }} </p>
   </div>
 </template>
 
@@ -15,7 +33,13 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App!'
+      method: "",
+      input: ""
+    }
+  },
+  computed: {
+    output() {
+      return this.input.toUpperCase();
     }
   }
 }
@@ -23,5 +47,46 @@ export default {
 
 <style lang="scss">
   #app {
+    padding: 20px;
   }
+  .settings {
+    display: flex;
+    border: 1px solid black;
+    
+    input {
+      display: none;
+    }
+      
+    label {
+      width: 50%;
+      text-align: center;
+      padding: 10px 0;
+      background: #fff;
+
+      &:hover {
+        background: rgb(236, 237, 240);
+      }
+
+      &:last-child {
+        border-left: 1px solid #000;
+      }
+    }
+  
+    input:checked + label {
+      background: #D5D6DF;
+    }
+  }
+  
+  .settings + .settings {
+    border-top: none;
+  } 
+
+  textarea {
+    width: 100%;
+    resize: none;
+    border: 1px solid #000;
+    font-size: 14px;
+    padding: 10px;
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -5,3 +5,11 @@ new Vue({
   el: '#app',
   render: h => h(App)
 });
+
+function calcVH () {
+	var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	document.querySelector('#app').setAttribute("style", "height:" + vH + "px;");
+}
+window.onload = calcVH();
+window.addEventListener('onorientationchange', calcVH, true);
+window.addEventListener('resize', calcVH, true);
